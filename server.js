5a8cbfbf3dd34c7a7e6723891619e2f482c7549a
Emilio <emiliogzz24@gmail.com>
2020-05-04 23:52:05 -0500

    Wrapping up all the functionality

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const morgan = require( 'morgan' );
 const uuid = require('uuid');
+const mongoose = require( 'mongoose' );
 const validateToken = require('./middleware/validateToken');
+const { Bookmarks } = require('./models/bookmarkModel')
 const { check, validationResult } = require('express-validator');
+const {DATABASE_URL, PORT} = require( './config' );
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -11,32 +14,18 @@ const jsonParser = bodyParser.json();
 app.use(morgan('dev'));
 app.use(validateToken)
 
-let listOfPosts = [
-    {
-        id : uuid.v4(),
-        title : "Example post 1",
-        description: "Example description 1",
-        url: "https://www.google.com",
-        rating: 3
-    },
-    {
-        id : uuid.v4(),
-        title : "Example post 2",
-        description: "Example description 2",
-        url: "https://www.google.com",
-        rating: 3
-    },
-    {
-        id : uuid.v4(),
-        title : "Example post 3",
-        description: "Example description 3",
-        url: "https://www.google.com",
-        rating: 3
-    },
-];
-
 app.get( '/bookmarks', ( req, res ) => {
-    return res.status( 200 ).json( listOfPosts );
+    console.log("Entered the route...")
+    Bookmarks
+        .getAllBookmarks()
+        .then( result => {
+            return res.status( 200 ).json( result );
+        })
+        .catch( err => {
+            console.log(err);
+            res.statusMessage = "Something is wrong with the Database. Try again later.";
+            return res.status( 500 ).end();
+        })
 });
 
 app.post( '/bookmarks', jsonParser, [
@@ -50,7 +39,7 @@ app.post( '/bookmarks', jsonParser, [
         return res.status(406).json({ errors: errors.array({ onlyFirstError: true }) })
     }
 
-    let newPost = {
+    let newBookmark = {
         id : uuid.v4(),
         title : req.body.title,
         description: req.body.description,
@@ -58,23 +47,22 @@ app.post( '/bookmarks', jsonParser, [
         rating: req.body.rating
     }
 
-    listOfPosts.push(newPost)
 
-    return res.status( 201 ).json(newPost);
+    Bookmarks
+        .createBookmark(newBookmark)
+        .then(createdBookmark=> {
+            return res.status( 201 ).json(createdBookmark) 
+        })
+        .catch( err => {
+            console.log(err);
+            res.statusMessage = "Something is wrong with the Database. Try again later.";
+            return res.status( 500 ).end();
+        })
 });
-
-const findPostById = (id) => {
-    for (let i = 0; i<listOfPosts.length; i++) {
-        if (listOfPosts[i].id == id) {
-            return i;
-        }
-    }
-    return -1;
-}
 
 app.patch( '/bookmark/:id', jsonParser, [
     check('id').isString().not().isEmpty(),
-],( req, res ) => {
+], ( req, res ) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(406).json({ errors: errors.array({ onlyFirstError: true }) })
@@ -84,52 +72,68 @@ app.patch( '/bookmark/:id', jsonParser, [
         res.statusMessage = `The id in the body doesn't match the one on the params`;
         return res.status(409).end();
     }
-
-    let postIndex = findPostById(req.params.id);
-    if (postIndex != -1) {
-        let updatedPost = listOfPosts[postIndex];
-        for (key in req.body.object) {
-            updatedPost[key] = req.body.object[key];
-        }
-        listOfPosts[postIndex] = updatedPost;
-        return res.status(202).json(updatedPost);
-    } else {
-        res.statusMessage = `Wasn't able to find a post with the id ${req.params.id}`;
-        return res.status(404).end();
-    }
+    return Bookmarks
+        .updateBookmark(req.params.id, req.body.object)
+        .then((bookmark) => {
+            if (!bookmark) {
+                return res.status( 404 ).end();
+            }
+            return res.status(202).json(bookmark);
+        }).catch(err => {
+            console.log(err);
+            res.statusMessage = "Something is wrong with the Database. Try again later.";
+            return res.status(500).end();
+        })
 });
 
 app.delete( '/bookmark/:id', ( req, res ) => {
-    let postIndex = findPostById(req.params.id);
-    if (postIndex != -1) {
-        listOfPosts.splice(postIndex, 1);
-        return res.status(200).end();
-    } else {
-        res.statusMessage = `Wasn't able to find a post with the id ${req.params.id}`;
-        return res.status(404).end();
-    }
+    let bookmarkId = req.params.id;
+    Bookmarks
+        .deleteBookmark(bookmarkId)
+        .then(res => {
+            return res.status( 200 );
+        }).catch(err => {
+            return res.status( 500 ).end();
+        })
 });
 
 app.get( '/bookmark', ( req, res ) => {
-    console.log(req.query);
     var title = req.query.title;
-    console.log(title)
-    if (title) {
-        var foundPosts = listOfPosts.filter(post => post.title == title);
-        
-        if (foundPosts.length > 0) {
-            return res.status(200).json(foundPosts);
-        } else {
-            res.statusMessage = `The title ${title} wasn't found on any of our titles.`;
-            return res.status(404).end();
-        }
-        
-    } else {
+
+    if (!title) {
         res.statusMessage = 'A title is required in this endpoint.';
         return res.status(406).end();
     }
+
+    Bookmarks.getBookmark(title)
+    .then(bookmark => {
+        return res.status(200).json(bookmark);
+    }).catch((err) => {
+        res.statusMessage = `The title ${title} wasn't found on any of our titles.`;
+        return res.status(404).end();        
+    })
 });
 
-app.listen( 8080, () => {
+app.listen( PORT, () => {
     console.log( "This server is running on port 8080" );
+
+    new Promise( ( resolve, reject ) => {
+        const settings = {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            useCreateIndex: true
+        };
+        mongoose.connect( DATABASE_URL, settings, ( err ) => {
+            if( err ){
+                return reject( err );
+            }
+            else{
+                console.log( "Database connected successfully." );
+                return resolve();
+            }
+        })
+    })
+    .catch( err => {
+        console.log( err );
+    });
 });
